f5c772d474a8c4c51a190941d0bd9c9ebe1cdb92
A-Russell23 <avrrussell@gmail.com>
2023-03-21 12:19:46 +0000

    Added target dummy with health

---
+++ Source/TPProj/Private/Character/AICharacter.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AICharacter.h"
+
+#include "Character/Player/DemoPlayerCharacter.h"
+#include "AI/PlayerAIController.h"
+#include "Player/DemoPlayerState.h"
+#include "Player/DemoPlayerController.h"
+#include "Character/Abilities/CharacterAbilitySystemComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+AICharacter::AICharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	//GetMesh()->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::AlwaysTickPoseAndRefreshBones;
+	//GetMesh()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	//GetMesh()->SetCollisionProfileName(FName("NoCollision"));
+
+	AIControllerClass = APlayerController::StaticClass();
+
+	DeadTag = FGameplayTag::RequestGameplayTag(FName("State.Dead"));
+}
+
+void AICharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	ADemoPlayerState* PS = GetPlayerState<ADemoPlayerState>();
+	if (PS)
+	{
+		InitializeStartingValues(PS);
+		AddStartupEffects();
+		AddCharacterAbilities();
+	}
+}
+
+void AICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AICharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	ADemoPlayerState* PS = GetPlayerState<ADemoPlayerState>();
+	if (PS)
+	{
+		InitializeStartingValues(PS);
+	}
+}
+
+void AICharacter::InitializeStartingValues(ADemoPlayerState* PS)
+{
+	AbilitySystemComponent = Cast<UCharacterAbilitySystemComponent>(PS->GetAbilitySystemComponent());
+	PS->GetAbilitySystemComponent()->InitAbilityActorInfo(PS, this);
+	AttributeSetBase = PS->GetAttributeSetBase();
+	AbilitySystemComponent->SetTagMapCount(DeadTag, 0);
+	InitializeAttributes();
+	SetHealth(GetMaxHealth());
+	SetMana(GetMaxMana());
+}
