82971b844ed7470fd63272a78fa164356333a0b9
A-Russell23 <avrrussell@gmail.com>
2023-04-20 13:24:37 +0100

    Added water spell

--- Source/TPProj/TPProj.h
+++ Source/TPProj/TPProj.h
@@ -10,5 +10,6 @@ enum class TPProjAbilityID : uint8
 	None UMETA(DisplayName = "None"),
 	Confirm UMETA(DisplayName = "Confirm"),
 	Cancel UMETA(DisplayName = "Cancel"),
-	Ability1 UMETA(DisplayName = "Ability1")
+	Ability1 UMETA(DisplayName = "Ability1"),
+	Ability2 UMETA(DisplayName = "Ability2")
 };
